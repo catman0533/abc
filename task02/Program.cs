bfebfe799f2c2c245260b5bd5db8c43fced9e731
yuriy <catman0533@gmail.com>
2023-03-07 16:41:05 +0300

    added task00

---
+++ task02/Program.cs
@@ -0,0 +1,14 @@
+﻿           Console.WriteLine("Введите число а");
+           int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число в");
+           int b = Convert.ToInt32(Console.ReadLine());
+
+            if(a > b)
+            {
+                Console.WriteLine($"max = {a}");
+            } else
+            {
+                Console.WriteLine($"max = {b}");
+            }
+
+            
