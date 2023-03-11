ac1b2e53fac611fb91f3576f2c3373e50496a416
yuriy <catman0533@gmail.com>
2023-03-11 21:46:38 +0300

    added task10

---
+++ TASK10/Program.cs
@@ -0,0 +1,11 @@
+﻿
+// int number = new Random(), Naxt, (100,1000)
+// Console.WriteLine ($"Слуыайно число из отрезка 100и-1000 -> {number}");
+
+int number = new Random().Next( 100,1000);
+Console.WriteLine($" Случайное число из отрезка 100 -1000  -> { number}");
+
+
+int SecondDigit = (number /10)%10 ;
+Console.WriteLine($"Вторая цифра числа -> {SecondDigit}");
+
