f771b2de981a756d479ada527e5943fec111ff41
yuriy <catman0533@gmail.com>
2023-03-15 21:22:17 +0300

    added task15

---
+++ task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine( "Введите номер дня недели");
+int num = Convert.ToInt32( Console.ReadLine());
+
+// if(num >= 1 && num <= 7 ) (num == 6 || num == 7);
+
+if(num == 6 | num == 7)
+Console.WriteLine(" Сегодня выходной");
+
+
+ else
+ {
+    Console.WriteLine(" Сегодня рабочий день"); 
+ } 
+
+ 
+  
+          
+   
+      
