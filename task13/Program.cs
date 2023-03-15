93c9f50284392cba1bb486ba4fc9662eb5b5ae32
yuriy <catman0533@gmail.com>
2023-03-15 20:21:18 +0300

    added task13

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// number = number/10 ;
+// int ThirdDigit = number%10;
+
+if( number > 100 )
+ {
+    int ThirdDigit = number%10;
+    Console.WriteLine($"Третья цифра -> {ThirdDigit}");
+ }       
+    
+
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
+
+   
