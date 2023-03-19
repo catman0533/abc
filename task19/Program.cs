a748efbe1bef8d2a964d6734378e334ddb1dc888
yuriy <catman0533@gmail.com>
2023-03-19 09:25:42 +0300

    added task19

---
+++ task19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine(" ВВедите пятизначное чичло");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number/10000;
+int secondDigit = (number/1000)%10;
+int fourthDigit = (number/10)%10;
+int fifthDigit =  number%10;
+
+if(firstDigit == fifthDigit && secondDigit == fourthDigit )
+{
+    Console.WriteLine( $"{number} -это число полиндром");
+}
+else
+{
+    Console.WriteLine($"{number}- не полиндром");
+}
+
