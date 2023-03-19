62a8f51c48576c0ae5df40415af9fca3141abcc1
yuriy <catman0533@gmail.com>
2023-03-19 11:20:21 +0300

    added task23

---
+++ task23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableCube(number);
+void TableCube(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+       Console.WriteLine($"{i, 4} -> {i*i*i, 4}");
+       i++; 
+    }
+}
