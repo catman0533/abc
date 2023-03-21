0f793a00e6693c01d40a3366a1459243bdfe6e2c
yuriy <catman0533@gmail.com>
2023-03-21 23:04:21 +0300

    added task27

---
+++ task27/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumdigit = SumDigit(number);
+Console.WriteLine($" Сумма цифр в числе = {sumdigit}");
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number/=10;
+    }
+    return sum;
+}
