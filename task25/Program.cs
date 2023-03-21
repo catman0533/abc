7a1be1af0534055dffaf768232b79408539d81ce
yuriy <catman0533@gmail.com>
2023-03-21 18:02:15 +0300

    added task25

---
+++ task25/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int todegree = ToDegree (numberA, numberB);
+Console.WriteLine($"Число A в степени B = {todegree} ");
+
+int ToDegree(int a, int b)
+{
+    int res = 1;
+    for (int i = 1; i <= numberB; i++)
+
+        res *= numberA;
+    return res;
+}  
+
+
+
+
