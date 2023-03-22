2f88e589f263099781ee7158312e90ed0099e4c4
yuriy <catman0533@gmail.com>
2023-03-22 19:42:40 +0300

    added task29

---
+++ task29/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[8];
+
+FillArray(array);
+WriteArray(array);
+
+void FillArray(int[] arr)
+{
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0, 19);
+    }
+}
+void WriteArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
