578120199ac2d1933709fc25bf348c157af40adf
yuriy <catman0533@gmail.com>
2023-04-01 19:11:42 +0300

    added task43

---
+++ task43/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Ввведите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввведите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввведите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввведите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые паралельны, нет точки пересечения");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+}
+Console.WriteLine($"Точка пересеченлия :({x},{y})");
+
+
+
