43e640fdb27aa9df5e5d1a9b2f9f18313c1e91c6
yuriy <catman0533@gmail.com>
2023-04-04 14:11:16 +0300

    added task51

---
+++ task51/Program.cs
@@ -0,0 +1,57 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+
+{
+
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        Console.WriteLine("|");
+    }
+
+
+}
+
+int SumElementOfDigonal(int[,] matrix)
+{
+    int sum = 0;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+    return sum;
+}  
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 100);
+PrintMatrix(array2d);
+Console.WriteLine();
+int sumElementOfDigonal = SumElementOfDigonal(array2d);
+Console.WriteLine($"{sumElementOfDigonal}");
