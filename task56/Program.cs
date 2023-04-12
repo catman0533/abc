3ae1d85544002caed93ca3f4e339409adff15ede
yuriy <catman0533@gmail.com>
2023-04-12 19:34:49 +0300

    added task56

---
+++ task56/Program.cs
@@ -0,0 +1,63 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+
+    int[,] matrix = new int[rows, columns];
+
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 100);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+// SortToLower(array2d);
+// InputNumbers(array2d)
+
+
+int minSumLine = 0;
+int sumLine = SumLineElements(array2d, 0);
+for (int i = 1; i < array2d.GetLength(0); i++)
+{
+  int tempSumLine = SumLineElements(array2d, i);
+  if (sumLine > tempSumLine)
+  {
+    sumLine = tempSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumLineElements(int[,] array2d, int i)
+{
+  int sumLine = array2d[i,0];
+  for (int j = 1; j < array2d.GetLength(1); j++)
+  {
+    sumLine += array2d[i,j];
+  }
+  return sumLine;
+}
